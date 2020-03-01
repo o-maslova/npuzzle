4b49ac28f1035ca62e6709c3f24a2f4409a63c7e
Oleksandra MASLOVA <omaslova@e1r3p9.unit.ua>
2020-03-01 20:51:06 +0200

    [ADD] combine algo and parsing in main

--- algorithm/main.py
+++
@@ -1,7 +0,0 @@
-import sys
-from aStarSearch import a_star
-
-
-if __name__ == "__main__":
-    arr = [[0, 5, 7], [3, 2, 4], [1, 8, 6]]
-    a_star(arr)
--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import sys
+from algorithm.aStarSearch import a_star
 from parser import parsing
 
 
 if __name__ == "__main__":
     if len(sys.argv) == 2:
-        parsing(sys.argv[1])
+        arr = parsing(sys.argv[1])
+        a_star(arr)
     else:
         print("usage: npuzzle.py 'filename'")
         print("No file!")
+    # arr = [[6, 23, 21, 10, 0], [18, 22, 20, 11, 24], [14, 3, 13, 9, 15], [2, 12, 17, 4, 19], [7, 5, 8, 1, 16]]
+    # arr = [[8, 12, 7, 4], [5, 6, 0, 14], [3, 13, 15, 9], [1, 11, 10, 2]]
+    # arr = [[0, 5, 7], [3, 2, 4], [1, 8, 6]]
